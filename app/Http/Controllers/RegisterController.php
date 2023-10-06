34facb42cbb84d27892841ad0722ee901ec59186
rayray2324 <122052273+rayray2324@users.noreply.github.com>
2023-10-05 21:55:16 -0300

    acrescentando o campo tipo usuario

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 // use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -15,9 +16,16 @@ class RegisterController extends Controller
     public function store()
     {
         $attributes = request()->validate([
-            'username' => 'required|max:255|min:2',
-            'email' => 'required|email|max:255|unique:users,email',
-            'password' => 'required|min:5|max:255',
+            'username' => ['required','max:255', 'min:2'],
+            'firstname' => ['max:100'],
+            'lastname' => ['max:100'],
+            'email' => ['required', 'email', 'max:255',  Rule::unique('users')->ignore(auth()->user()->id),],
+            'address' => ['max:100'],
+            'city' => ['max:100'],
+            'country' => ['max:100'],
+            'postal' => ['max:100'],
+            'about' => ['max:255'],
+            'clientType' => ['max:1'],
             'terms' => 'required'
         ]);
         $user = User::create($attributes);
