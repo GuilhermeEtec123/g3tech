2d6c4914fad48af3e19c76a7bbf8882e8c7ce665
rayray2324 <122052273+rayray2324@users.noreply.github.com>
2023-10-26 21:46:11 -0300

    correção do update do user

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
-
+use App\Models\User as userModel;
 class UserProfileController extends Controller
 {
     public function show()
@@ -26,17 +26,9 @@ class UserProfileController extends Controller
             'about' => ['max:255']
         ]);
 
-        auth()->user()->update([
-            'username' => $request->get('username'),
-            'firstname' => $request->get('firstname'),
-            'lastname' => $request->get('lastname'),
-            'email' => $request->get('email') ,
-            'address' => $request->get('address'),
-            'city' => $request->get('city'),
-            'country' => $request->get('country'),
-            'postal' => $request->get('postal'),
-            'about' => $request->get('about')
-        ]);
+        auth()->UserModel->update($attributes);
         return back()->with('succes', 'Profile succesfully updated');
     }
+
+
 }
