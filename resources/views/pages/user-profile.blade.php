7ec8039c7f840b3f134aed19d1a20571e5594244
Guilherme <122052273+rayray2324@users.noreply.github.com>
2023-10-31 19:43:50 -0300

    criação da parte de curriculo

--- resources/views/pages/user-profile.blade.php
+++ resources/views/pages/user-profile.blade.php
@@ -137,66 +137,77 @@
                     </form>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card card-profile">
-                    <img src="/img/bg-profile.jpg" alt="Image placeholder" class="card-img-top">
-                    <div class="row justify-content-center">
-                        <div class="col-4 col-lg-4 order-lg-2">
-                            <div class="mt-n4 mt-lg-n6 mb-4 mb-lg-0">
-                                <a href="javascript:;">
-                                    <img src="/img/team-2.jpg"
-                                        class="rounded-circle img-fluid border border-2 border-white">
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-header text-center border-0 pt-0 pt-lg-2 pb-4 pb-lg-3">
-                        <div class="d-flex justify-content-between">
-                            <a href="javascript:;" class="btn btn-sm btn-info mb-0 d-none d-lg-block">Connect</a>
-                            <a href="javascript:;" class="btn btn-sm btn-info mb-0 d-block d-lg-none"><i
-                                    class="ni ni-collection"></i></a>
-                            <a href="javascript:;"
-                                class="btn btn-sm btn-dark float-right mb-0 d-none d-lg-block">Message</a>
-                            <a href="javascript:;" class="btn btn-sm btn-dark float-right mb-0 d-block d-lg-none"><i
-                                    class="ni ni-email-83"></i></a>
-                        </div>
-                    </div>
-                    <div class="card-body pt-0">
+            <hr class="horizontal dark">
+                        <p class="text-uppercase text-sm">Currículo</p>
                         <div class="row">
-                            <div class="col">
-                                <div class="d-flex justify-content-center">
-                                    <div class="d-grid text-center">
-                                        <span class="text-lg font-weight-bolder">22</span>
-                                        <span class="text-sm opacity-8">Friends</span>
-                                    </div>
-                                    <div class="d-grid text-center mx-4">
-                                        <span class="text-lg font-weight-bolder">10</span>
-                                        <span class="text-sm opacity-8">Photos</span>
-                                    </div>
-                                    <div class="d-grid text-center">
-                                        <span class="text-lg font-weight-bolder">89</span>
-                                        <span class="text-sm opacity-8">Comments</span>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="example-text-input" class="form-control-label">Resumo de minha experiência profissional</label>
+                                    <textarea name="resumoExperienciaProfissional" class="form-control" placeholder="Resumo de minha experiência profissional" aria-label="Resumo de minha experiência profissional" rows="5" maxlength="500">{{ old('resumoExperienciaProfissional') }}</textarea>
+                                </div>
+                            </div>
+
+
+
+                            <hr class="horizontal dark">
+                            <p class="text-uppercase text-sm">Habilidades</p>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label class="form-control-label">Selecione suas habilidades:</label>
+                                        <ul class="skill-options">
+                                            <li>
+                                                <label for="habilidadeDesenvolvimentoWeb">Desenvolvimento Web</label>
+                                            </li>
+                                            <li>
+                                                <label for="habilidadeEscritaWeb">Escrita para Web</label>
+                                            </li>
+                                            <li>
+                                                <label for="habilidadeEstrategiaMarketing">Estratégia de Marketing</label>
+                                            </li>
+                                            <li>
+                                                <label for="habilidadeEstrategiaNegocios">Estratégia de Negócios</label>
+                                            </li>
+                                            <li>
+                                                <label for="habilidadeGerenciamentoVendas">Gerenciamento de Vendas</label>
+                                            </li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="text-center mt-4">
-                            <h5>
-                                Mark Davis<span class="font-weight-light">, 35</span>
-                            </h5>
-                            <div class="h6 font-weight-300">
-                                <i class="ni location_pin mr-2"></i>Bucharest, Romania
-                            </div>
-                            <div class="h6 mt-4">
-                                <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
-                            </div>
-                            <div>
-                                <i class="ni education_hat mr-2"></i>University of Computer Science
+
+
+                            <hr class="horizontal dark">
+                            <p class="text-uppercase text-sm">Áreas de interesse</p>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label class="form-control-label">Selecione suas áreas de interesse:</label>
+                                        <ul class="skill-options">
+                                            <li>
+                                                <label for="habilidadeDesenvolvimentoWeb">Desenvolvimento Web</label>
+                                            </li>
+                                            <li>
+                                                <label for="habilidadeEscritaWeb">Escrita para Web</label>
+                                            </li>
+                                            <li>
+                                                <label for="habilidadeEstrategiaMarketing">Estratégia de Marketing</label>
+                                            </li>
+                                            <li>
+                                                <label for="habilidadeEstrategiaNegocios">Estratégia de Negócios</label>
+                                            </li>
+                                            <li>
+                                                <label for="habilidadeGerenciamentoVendas">Gerenciamento de Vendas</label>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
         @include('layouts.footers.auth.footer')
     </div>
