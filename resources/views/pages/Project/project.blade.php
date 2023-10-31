a77738f888ae2350c62ad8d4eae8147be2506f22
Guilherme <122052273+rayray2324@users.noreply.github.com>
2023-10-30 21:25:22 -0300

    tela de detalhes do projeto

--- resources/views/pages/Project/project.blade.php
+++ resources/views/pages/Project/project.blade.php
@@ -3,5 +3,5 @@
 <p>Criado por: {{ $projeto->cliente->firstname }} {{ $projeto->cliente->lastname }}</p>
 
 <p>{{$projeto->titulo}}</p>
-<p> {{$projeto->orcamento}}</p>
+<p>{{$projeto->orcamento}}</p>
 <p>Descrição: {{ $projeto->descricao }}</p> 
