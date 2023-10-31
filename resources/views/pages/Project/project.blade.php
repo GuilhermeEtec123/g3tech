0f8bbdf05ea20dcb8d1aa410c7be3013ddb2ee80
unknown <icaro>
2023-10-30 22:23:24 -0300

    implementação dos novos campos

--- resources/views/pages/Project/project.blade.php
+++ resources/views/pages/Project/project.blade.php
@@ -5,3 +5,10 @@
 <p>{{$projeto->titulo}}</p>
 <p> {{$projeto->orcamento}}</p>
 <p>Descrição: {{ $projeto->descricao }}</p> 
+<p>Prazo: {{ $projeto->prazo }} </p>
+<p>Quantidade de prestadores: {{ $projeto->qtdPrestadores}}
+<p>Data Atual: {{ $projeto->data_atual}}</p>
+<p>Criado em: {{ $projeto->created_at}}</p>
+
+
+ 
