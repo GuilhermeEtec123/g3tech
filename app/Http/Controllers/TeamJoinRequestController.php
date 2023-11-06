2ccb9a17d2cf2d1e4c3e1b7f6d55b9109b4f5a27
Guilherme <122052273+rayray2324@users.noreply.github.com>
2023-11-06 19:39:51 -0300

    ajuste da solicitação

--- app/Http/Controllers/TeamJoinRequestController.php
+++ app/Http/Controllers/TeamJoinRequestController.php
@@ -1,47 +1,101 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\TeamJoinRequest;
+
+use App\Models\TeamJoinRequest as TeamJoinRequestModel;
+use App\Models\Equipe as EquipeModel;
+use App\Models\User as UserModel;
+use App\Models\Projeto as ProjetoModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TeamJoinRequestController extends Controller
 {
-    
+
     public function index()
     {
-        // Recupere todas as solicitações pendentes
-        // $solicitacoesPendentes = TeamJoinRequest::where('status', 'pending')->get();
+        $solicitacoesPendentes = TeamJoinRequestModel::where('status', 'pendente')
+            ->with('freelancer') // Carregue os dados do freelancer
+            ->get();   
+        return view('pages.Team.solicitacao', ['solicitacoesPendentes' => $solicitacoesPendentes]);
+    }
+    
 
-        // return view('solicitacoes.index', ['solicitacoesPendentes' => $solicitacoesPendentes]);
-        return view('Team.solicitacao');
+    public function show()
+    {
+        $solicitacoesPendentes = TeamJoinRequestModel::orderBy('created_at', 'desc')->get();
+        return view('pages.Team.solicitacao-list', compact('solicitacoesPendentes'));
+    }
+
+    public function create()
+    {
+        // Crie a view para enviar solicitações de participação
+        return view('team_join_requests.create');
+    }
+
+    public function store($projetoId)
+    {
+
+        $equipe = EquipeModel::where('projeto_id', $projetoId)->first();
+        // $projeto = ProjetoModel::find($projetoId);
+
+        if ($equipe) {
+            $equipeId = $equipe->id;
+            $teamJoinRequest = new TeamJoinRequestModel([
+                'freelancer_id' => Auth::id(),
+                'team_id' => $equipeId,
+                'status' => 'pendente'
+            ]);
+
+            $teamJoinRequest->save();
+
+            return back()->with('success', 'Solicitação enviada com sucesso.');
+
+            // return redirect()->route('solicitacoes');
+            // return view('pages.Team.solicitacao');
+        } else {
+            return back()->with('success', 'Solicitação enviada com erro.');
+        }
+
+        return back()->with('success', 'Solicitação enviada com sucesso.');
     }
 
 
+    public function aceitar($id)
+    {
+        $solicitacao = TeamJoinRequestModel::find($id);
+        if ($solicitacao) {
+            $solicitacao->status = 'aceito';
+            $solicitacao->save();
 
-    // public function aceitar($id)
-    // {
-    //     $solicitacao = TeamJoinRequest::find($id);
-    //     if ($solicitacao) {
-    //         $solicitacao->status = 'accepted';
-    //         $solicitacao->save();
-    //         // Implemente a lógica para associar o ClienteFreelancer à equipe correspondente
+            // Obtenha o ID da equipe da solicitação
+            $teamId = $solicitacao->team_id;
 
-    //         return redirect()->route('solicitacoes.index')->with('success', 'Solicitação aceita com sucesso.');
-    //     }
-    //     return redirect()->route('solicitacoes.index')->with('error', 'Solicitação não encontrada.');
-    // }
+            // Obtenha o ID do freelancer da solicitação
+            $freelancerId = $solicitacao->user_id;
 
-    // public function rejeitar($id)
-    // {
-    //     $solicitacao = TeamJoinRequest::find($id);
-    //     if ($solicitacao) {
-    //         $solicitacao->status = 'rejected';
-    //         $solicitacao->save();
+            // Crie um novo registro na tabela de equipes para associar o freelancer à equipe
+            $equipe = new EquipeModel();
+            $equipe->projeto_id = $teamId; // Defina o ID do projeto, se aplicável
+            $equipe->membro_id = Auth::id();
+            // $equipe->cargo_equipe = 'Membro'; // Defina o cargo conforme necessário
+            $equipe->save();
+            // Implemente a lógica para associar o ClienteFreelancer à equipe correspondente
 
-    //         return redirect()->route('solicitacoes.index')->with('success', 'Solicitação rejeitada com sucesso.');
-    //     }
-    //     return redirect()->route('solicitacoes.index')->with('error', 'Solicitação não encontrada.');
-    // }
+            return redirect()->route('solicitacoes')->with('success', 'Solicitação aceita com sucesso.');
+        }
+        return redirect()->route('solicitacoes')->with('error', 'Solicitação não encontrada.');
+    }
 
+    public function rejeitar($id)
+    {
+        $solicitacao = TeamJoinRequestModel::find($id);
+        if ($solicitacao) {
+            $solicitacao->status = 'rejeitado';
+            $solicitacao->save();
 
+            return redirect()->route('solicitacoes')->with('success', 'Solicitação rejeitada com sucesso.');
+        }
+        return redirect()->route('solicitacoes')->with('error', 'Solicitação não encontrada.');
+    }
 }
--- app/Models/TeamJoinRequest.php
+++ app/Models/TeamJoinRequest.php
@@ -1,11 +1,32 @@
 <?php
+// app/Models/TeamJoinRequest.php
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TeamJoinRequest extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'freelancer_id',
+        'team_id',
+        'status'
+    ];
+    protected $table = 'solicitacoes';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'cliente_id');
+    }
+
+    public function equipe()
+    {
+        return $this->belongsTo(Equipe::class, 'team_id');
+    }
+
+    public function freelancer()
+    {
+        return $this->belongsTo(User::class, 'freelancer_id', 'id');
+    }
+    
 }
