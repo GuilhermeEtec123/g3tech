ce42ac6bec669ca81454a9ccb7c94d4cbece4e46
Guilherme <122052273+rayray2324@users.noreply.github.com>
2023-11-06 19:42:32 -0300

    chamando o titulo do projeto

--- resources/views/pages/Team/team.blade.php
+++ resources/views/pages/Team/team.blade.php
@@ -1,60 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Equipe'])
-    <div class="row mt-4 mx-4">
-        <div class="col-12">
-            <div class="card mb-4">
-                <div class="card-header pb-0 d-flex justify-content-between">
-                    <h6>Equipe</h6>
-                    <h6>Nome Projeto</h6>
-                </div>
-                <div class="card-body px-0 pt-0 pb-2">
-                    <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
-                            <thead>
-                                <tr>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">Nome</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Função</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Telefone</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Email</th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Create Date</th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <div class="d-flex px-3 py-1 justify-content-center">
-                                            <div class="d-flex flex-column justify-content-center">
-                                                <h6 class="mb-0 text-sm">Guilherme</h6>
-                                            </div>
+@include('layouts.navbars.auth.topnav', ['title' => 'Equipe'])
+<div class="row mt-4 mx-4">
+    <div class="col-12">
+        <div class="card mb-4">
+            <div class="card-header pb-0 d-flex justify-content-between">
+                <h6>Equipe</h6>
+                <h6>{{$projeto->titulo}}</h6>
+            </div>
+            <div class="card-body px-0 pt-0 pb-2">
+                <div class="table-responsive p-0">
+                    <table class="table align-items-center mb-0">
+                        <thead>
+                            <tr>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">Nome</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Função</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Telefone</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Email</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    Create Date</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    Action</th>
+                            </tr>
+                        </thead>
+                        @foreach ($membros as $membro)
+                        <p>{{ $membro->nome }}</p>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div class="d-flex px-3 py-1 justify-content-center">
+                                        <div class="d-flex flex-column justify-content-center">
+                                            <h6 class="mb-0 text-sm">{{ $membro->firstname }}</h6>
                                         </div>
-                                    </td>
-                                    <td>
-                                        <p class="text-sm font-weight-bold mb-0 text-center">Front-end</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm">
-                                        <p class="text-sm font-weight-bold mb-0">(11)99425-9127</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm">
-                                        <p class="text-sm font-weight-bold mb-0">email@email.com</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm">
-                                        <p class="text-sm font-weight-bold mb-0">10/07/1998</p>
-                                    </td>       
-                                    <td class="align-middle text-end">
-                                        <div class="d-flex px-3 py-1 justify-content-center align-items-center">
-                                            <p class="text-sm font-weight-bold mb-0">Edit</p>
-                                            <p class="text-sm font-weight-bold mb-0 ps-2">Delete</p>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <!-- <tr>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-sm font-weight-bold mb-0 text-center">{{ $membro->freelancerType}}</p>
+                                </td>
+                                <td class="align-middle text-center text-sm">
+                                    <p class="text-sm font-weight-bold mb-0">(11)99425-9127</p>
+                                </td>
+                                <td class="align-middle text-center text-sm">
+                                    <p class="text-sm font-weight-bold mb-0">{{ $membro->email }}</p>
+                                </td>
+                                <td class="align-middle text-center text-sm">
+                                    <p class="text-sm font-weight-bold mb-0">10/07/1998</p>
+                                </td>
+                                <td class="align-middle text-end">
+                                    <div class="d-flex px-3 py-1 justify-content-center align-items-center">
+                                        <p class="text-sm font-weight-bold mb-0">Edit</p>
+                                        <p class="text-sm font-weight-bold mb-0 ps-2">Delete</p>
+                                    </div>
+                                </td>
+                            </tr>
+                            <!-- <tr>
                                     <td>
                                         <div class="d-flex px-3 py-1">
                                             <div>
@@ -102,11 +102,12 @@
                                         </div>
                                     </td>
                                 </tr> -->
-                            </tbody>
-                        </table>
-                    </div>
+                        </tbody>
+                        @endforeach
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
