e6132fc2b79d2d9a093fe1b00fd6804e5d7b978d
Guilherme <122052273+rayray2324@users.noreply.github.com>
2023-11-06 19:43:56 -0300

    ajuste na tela de solicitação

--- resources/views/pages/Team/solicitacao.blade.php
+++ resources/views/pages/Team/solicitacao.blade.php
@@ -3,142 +3,50 @@
 @section('content')
 @include('layouts.navbars.auth.topnav', ['title' => 'Solicitações Pendentes'])
 
-    <div class="container">
-        <h1>Solicitações Pendentes</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Freelancer</th>
-                    <th>Equipe</th>
-                    <th>Status</th>
-                    <th>Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- @foreach ($solicitacoesPendentes as $solicitacao) -->
-                    <tr>
-                        <!-- <td>{{ $solicitacao->freelancer->nome }}</td>
-                        <td>{{ $solicitacao->team->nome }}</td>
-                        <td>{{ $solicitacao->status }}</td>
-                        <td>
-                            <a href="{{ route('solicitacoes.aceitar', $solicitacao->id) }}">Aceitar</a>
-                            <a href="{{ route('solicitacoes.rejeitar', $solicitacao->id) }}">Rejeitar</a>
-                        </td> -->
-                        <td>nome</td>
-                        <td>equipe</td>
-                        <td>status</td>
-                        <td>
-                            <a href="{{ route('solicitacoes.aceitar', $solicitacao->id) }}">Aceitar</a>
-                            <a href="{{ route('solicitacoes.rejeitar', $solicitacao->id) }}">Rejeitar</a>
-                        </td> 
-                    </tr>
-                <!-- @endforeach -->
-            </tbody>
-        </table>
-    </div>
+<div class="row mt-4 mx-4">
+    <div class="col-12">
+        <div class="card mb-4">
+            <div class="card-body px-0 pt-0 pb-2">
+                <div class="table-responsive p-0">
+                    <table class="table align-items-center mb-0">
+                        <thead>
+                            <tr>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">Freelancer</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Equipe</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Status</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Ações</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    Create Date</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                    Action</th>
+                            </tr>
+                        </thead>
+                        @foreach ($solicitacoesPendentes as $solicitacao)
+                        <tbody class="text-center">
+                            <td>{{ $solicitacao->freelancer->firstname }}</td>
+                            <td>{{ $solicitacao->team_id }}</td>
+                            <td>{{ $solicitacao->status }}</td>
+                            <td></td>
+                            <td></td>
+                            <td class="d-flex justify-content-center">
+                                <form action="{{ route('solicitacoes.rejeitar', $solicitacao->id) }}" method="post" class="me-3">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="btn">Recusar</button>
+                                </form>
+                                <form action="{{ route('solicitacoes.aceitar', $solicitacao->id) }}" method="post">
+                                    @csrf
+                                    @method('post')
+                                    <button type="submit" class="btn btn-primary">Aceitar</button>
+                                </form>
+                            </td>
 
-
-    <!-- <div class="row mt-4 mx-4">
-        <div class="col-12">
-            <div class="card mb-4">
-                <div class="card-header pb-0">
-                    <h6>Solicitações</h6>
-                </div>
-                <div class="card-body px-0 pt-0 pb-2">
-                    <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
-                            <thead>
-                                <tr>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">Nome</th>
-
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Função</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Telefone</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">E-mail</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Status</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Ações</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <div class="d-flex px-3 py-1 justify-content-center">
-                                            <div class="d-flex flex-column justify-content-center">
-                                                <h6 class="mb-0 text-sm">Guilherme</h6>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="text-sm font-weight-bold mb-0 text-center">Front-end</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm text-center">
-                                        <p class="text-sm font-weight-bold mb-0">(11)99425-9127</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm text-center">
-                                        <p class="text-sm font-weight-bold mb-0">email@email.com</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm text-center">
-                                        <p class="text-sm font-weight-bold mb-0">10/07/1998</p>
-                                    </td>       
-                                    <td class="align-middle text-end">
-                                        <div class="d-flex px-3 py-1 justify-content-center align-items-center">
-                                            <p class="text-sm font-weight-bold mb-0">Edit</p>
-                                            <p class="text-sm font-weight-bold mb-0 ps-2">Delete</p>
-                                        </div>
-                                    </td>
-                                </tr> -->
-                                <!-- <tr>
-                                    <td>
-                                        <div class="d-flex px-3 py-1">
-                                            <div>
-                                                <img src="./img/team-2.jpg" class="avatar me-3" alt="image">
-                                            </div>
-                                            <div class="d-flex flex-column justify-content-center">
-                                                <h6 class="mb-0 text-sm">Creator</h6>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="text-sm font-weight-bold mb-0">Creator</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm">
-                                        <p class="text-sm font-weight-bold mb-0">22/03/2022</p>
-                                    </td>
-                                    <td class="align-middle text-end">
-                                        <div class="d-flex px-3 py-1 justify-content-center align-items-center">
-                                            <p class="text-sm font-weight-bold mb-0">Edit</p>
-                                            <p class="text-sm font-weight-bold mb-0 ps-2">Delete</p>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <div class="d-flex px-3 py-1">
-                                            <div>
-                                                <img src="./img/team-3.jpg" class="avatar me-3" alt="image">
-                                            </div>
-                                            <div class="d-flex flex-column justify-content-center">
-                                                <h6 class="mb-0 text-sm">Member</h6>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="text-sm font-weight-bold mb-0">Member</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm">
-                                        <p class="text-sm font-weight-bold mb-0">22/03/2022</p>
-                                    </td>
-                                    <td class="align-middle text-end">
-                                        <div class="d-flex px-3 py-1 justify-content-center align-items-center">
-                                            <p class="text-sm font-weight-bold mb-0 cursor-pointer">Edit</p>
-                                            <p class="text-sm font-weight-bold mb-0 ps-2 cursor-pointer">Delete</p>
-                                        </div>
-                                    </td>
-                                </tr> -->
-                            </tbody>
-                        </table>
-                    </div>
+                        </tbody>
+                        @endforeach
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
