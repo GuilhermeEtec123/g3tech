a9151c886bd71ad211f9bac1ea92b6aa83719004
Guilherme <122052273+rayray2324@users.noreply.github.com>
2023-11-06 19:35:00 -0300

    ajuste do index controller e model

--- app/Http/Controllers/EquipeController.php
+++ app/Http/Controllers/EquipeController.php
@@ -4,14 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Equipe;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class EquipeController extends Controller
 {
     public function index()
     {
-        $equipes = Equipe::all();
-        return view('pages.Team.index', compact('equipes'));
+        // Recupere o usuário autenticado
+        $user = Auth::user();
+    
+        // Inicialize uma variável para armazenar o projeto associado ao usuário
+        $projeto = null;
+    
+        // Inicialize um array para armazenar os membros da equipe associados ao projeto
+        $membros = [];
+    
+        // Recupere todas as equipes às quais o usuário pertence
+        $equipes = Equipe::where('membro_id', $user->id)->get();
+    
+        // Itere sobre as equipes para encontrar o projeto associado
+        foreach ($equipes as $equipe) {
+            if ($equipe->projeto) {
+                // Se a equipe estiver associada a um projeto, atribua o projeto à variável $projeto
+                $projeto = $equipe->projeto;
+    
+                // Recupere os membros da equipe associados a esse projeto
+                $equipesDoProjeto = Equipe::where('projeto_id', $projeto->id)->get();
+                foreach ($equipesDoProjeto as $equipeDoProjeto) {
+                    $membro = User::find($equipeDoProjeto->membro_id);
+                    $membros[] = $membro;
+                }
+    
+                // Saia do loop, pois já encontrou o projeto associado
+                break;
+            }
+        }
+    
+        return view('pages.Team.team', compact('projeto', 'membros'));
     }
+    
+
+
+
 
     public function create()
     {
--- app/Models/Equipe.php
+++ app/Models/Equipe.php
@@ -7,16 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Equipe extends Model
 {
- 
     protected $fillable = [
         'id',
         'projeto_id',
         'membro_id',
         'created_at'
-    ]; 
-    
-    public function cliente()
+    ];
+
+    // Defina o relacionamento entre a equipe e o projeto
+    public function projeto()
     {
-        return $this->belongsTo(User::class, 'cliente_id');
+        return $this->belongsTo(Projeto::class, 'projeto_id'); // Substitua 'Projeto' pelo nome correto da classe do modelo de projeto
+    }
+    
+    public function membro()
+    {
+        return $this->belongsTo(User::class, 'membro_id');
     }
 }
