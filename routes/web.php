4b48a6e938cc6cf1c0facba2cdfbb37375166c87
Guilherme <122052273+rayray2324@users.noreply.github.com>
2023-11-10 19:20:01 -0300

    rota

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,7 @@ Route::get('/reset-password', [ResetPassword::class, 'show'])->middleware('guest
 Route::post('/reset-password', [ResetPassword::class, 'send'])->middleware('guest')->name('reset.perform');
 Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('guest')->name('change-password');
 Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('guest')->name('change.perform');
-Route::get('/dashboard', [HomeController::class, 'index'])->name('home')->middleware('auth');
+// Route::get('/dashboard', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
 
 //solicitacoes
