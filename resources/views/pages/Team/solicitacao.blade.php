e1cef980d59e811a989283c6de549e7c8bab7865
Guilherme <122052273+rayray2324@users.noreply.github.com>
2023-11-10 14:35:46 -0300

    validação de usuario

--- resources/views/pages/Team/solicitacao.blade.php
+++ resources/views/pages/Team/solicitacao.blade.php
@@ -13,34 +13,36 @@
                             <tr>
                                 <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">Freelancer</th>
                                 <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Equipe</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Projeto</th>
                                 <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Status</th>
-                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Ações</th>
-                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                    Create Date</th>
-                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                    Action</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Data da solicitação</th>
+                                @if (Auth::user()->clientType == 1)
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Ações</th>
+                                @endif
                             </tr>
                         </thead>
                         @foreach ($solicitacoesPendentes as $solicitacao)
                         <tbody class="text-center">
                             <td>{{ $solicitacao->freelancer->firstname }}</td>
                             <td>{{ $solicitacao->team_id }}</td>
+                            <td>{{ $solicitacao->equipe->projeto->titulo }}</td>
                             <td>{{ $solicitacao->status }}</td>
-                            <td></td>
-                            <td></td>
-                            <td class="d-flex justify-content-center">
-                                <form action="{{ route('solicitacoes.rejeitar', $solicitacao->id) }}" method="post" class="me-3">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn">Recusar</button>
-                                </form>
-                                <form action="{{ route('solicitacoes.aceitar', $solicitacao->id) }}" method="post">
-                                    @csrf
-                                    @method('post')
-                                    <button type="submit" class="btn btn-primary">Aceitar</button>
-                                </form>
-                            </td>
-
+                            <td>{{ $solicitacao->created_at->format('d/m/Y') }}</td>
+                            
+                            @if (Auth::user()->clientType == 1)
+                                <td class="d-flex justify-content-center">
+                                    <form action="{{ route('solicitacoes.rejeitar', $solicitacao->id) }}" method="post" class="me-3">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn">Recusar</button>
+                                    </form>
+                                    <form action="{{ route('solicitacoes.aceitar', $solicitacao->id) }}" method="post">
+                                        @csrf
+                                        @method('post')
+                                        <button type="submit" class="btn btn-primary">Aceitar</button>
+                                    </form>
+                                </td>
+                            @endif
                         </tbody>
                         @endforeach
                     </table>
