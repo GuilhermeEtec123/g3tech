43426c89c8bde7add4a74cc6221950ea77dece32
Guilherme <122052273+rayray2324@users.noreply.github.com>
2023-11-10 14:34:54 -0300

    Alteração da logica para chamar o time

--- resources/views/pages/Team/team.blade.php
+++ resources/views/pages/Team/team.blade.php
@@ -1,128 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
-@include('layouts.navbars.auth.topnav', ['title' => 'Equipe'])
-<div class="row mt-4 mx-4">
-    <div class="col-12">
-        <div class="card mb-4">
-            <div class="card-header pb-0 d-flex justify-content-between">
-                <h6>Equipe</h6>
-                <h6>{{$projeto->titulo}}</h6>
-            </div>
-            <div class="card-body px-0 pt-0 pb-2">
-                <div class="table-responsive p-0">
-                    <table class="table align-items-center mb-0">
-                        <thead>
-                            <tr>
-                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">Nome</th>
-                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Função</th>
-                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Telefone</th>
-                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Email</th>
-                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                    Create Date</th>
-                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                    Action</th>
-                            </tr>
-                        </thead>
-                        @foreach ($membros as $membro)
-                        <p>{{ $membro->nome }}</p>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <div class="d-flex px-3 py-1 justify-content-center">
-                                        <div class="d-flex flex-column justify-content-center">
-                                            <h6 class="mb-0 text-sm">{{ $membro->firstname }}</h6>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <p class="text-sm font-weight-bold mb-0 text-center">{{ $membro->freelancerType}}</p>
-                                </td>
-                                <td class="align-middle text-center text-sm">
-                                    <p class="text-sm font-weight-bold mb-0">(11)99425-9127</p>
-                                </td>
-                                <td class="align-middle text-center text-sm">
-                                    <p class="text-sm font-weight-bold mb-0">{{ $membro->email }}</p>
-                                </td>
-                                <td class="align-middle text-center text-sm">
-                                    <p class="text-sm font-weight-bold mb-0">10/07/1998</p>
-                                </td>
-                                <td class="align-middle text-end">
-                                    <div class="d-flex px-3 py-1 justify-content-center align-items-center">
-                                    @if(Auth::user()->clientType == 1 || Auth::user()->freelancerType == 'Product owner')
-                                        <form action="{{ route('team-join-requests.store', ['projetoId' => $projeto->id]) }}" method="post">
-                                            @csrf
-                                            @method('post')
-                                            <input type="hidden" name="freelancer_id" value="{{ Auth::id() }}">
-                                            <button type="submit" class="btn btn-primary">Editar</button>
-                                        </form>
-                                    @endif
-
-                                    @if(Auth::user()->clientType == 1 || Auth::user()->freelancerType == 'Product owner')
-                                        <form action="{{ route('team-join-requests.store', ['projetoId' => $projeto->id]) }}" method="post">
-                                            @csrf
-                                            @method('post')
-                                            <input type="hidden" name="freelancer_id" value="{{ Auth::id() }}">
-                                            <button type="submit" class="btn">Excluir</button>
-                                        </form>
-                                    @endif
-                                    </div>
-                                </td>
-                            </tr>
-                            <!-- <tr>
-                                    <td>
-                                        <div class="d-flex px-3 py-1">
-                                            <div>
-                                                <img src="{{ asset('img/team-2.jpg') }}" class="avatar me-3" alt="image">
-                                            </div>
-                                            <div class="d-flex flex-column justify-content-center">
-                                                <h6 class="mb-0 text-sm">Creator</h6>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="text-sm font-weight-bold mb-0">Creator</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm">
-                                        <p class="text-sm font-weight-bold mb-0">22/03/2022</p>
-                                    </td>
-                                    <td class="align-middle text-end">
-                                        <div class="d-flex px-3 py-1 justify-content-center align-items-center">
-                                            <p class="text-sm font-weight-bold mb-0">Edit</p>
-                                            <p class="text-sm font-weight-bold mb-0 ps-2">Delete</p>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <div class="d-flex px-3 py-1">
-                                            <div>
-                                                <img src="./img/team-3.jpg" class="avatar me-3" alt="image">
-                                            </div>
-                                            <div class="d-flex flex-column justify-content-center">
-                                                <h6 class="mb-0 text-sm">Member</h6>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="text-sm font-weight-bold mb-0">Member</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm">
-                                        <p class="text-sm font-weight-bold mb-0">22/03/2022</p>
-                                    </td>
-                                    <td class="align-middle text-end">
-                                        <div class="d-flex px-3 py-1 justify-content-center align-items-center">
-                                            <p class="text-sm font-weight-bold mb-0 cursor-pointer">Edit</p>
-                                            <p class="text-sm font-weight-bold mb-0 ps-2 cursor-pointer">Delete</p>
-                                        </div>
-                                    </td>
-                                </tr> -->
-                        </tbody>
-                        @endforeach
-                    </table>
-                </div>
-            </div>
+    @include('layouts.navbars.auth.topnav', ['title' => 'Equipe'])
+    <div class="row mt-10 mx-4">
+        <div class="col-12">
+            @if(count($projetos) > 0)
+                @foreach ($projetos as $index => $projeto)
+                    <div class="card mb-4">
+                        <div class="card-header pb-0 d-flex justify-content-between">
+                            <h6>{{ $projeto->titulo }}</h6>
+                        </div>
+                        <div class="card-body px-0 pt-0 pb-2">
+                            <div class="table-responsive p-0">
+                                <table class="table align-items-center mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">Nome</th>
+                                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Função</th>
+                                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Telefone</th>
+                                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Email</th>
+                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                Data de criação</th>
+                                            @if (Auth::user()->clientType == 1)
+                                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Ações</th>
+                                            @endif
+                                        </tr>
+                                    </thead>
+                                    @foreach ($membrosPorProjeto[$projeto->id] as $membro)
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <div class="d-flex px-1 py-1 justify-content-center">
+                                                        <div class="d-flex flex-column justify-content-center">
+                                                            <h6 class="mb-0 text-sm">{{ $membro->firstname }}</h6>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <p class="text-sm font-weight-bold mb-0 text-center">{{ $membro->freelancerType }}</p>
+                                                </td>
+                                                <td class="align-middle text-center text-sm">
+                                                    <p class="text-sm font-weight-bold mb-0">(11)99425-9127</p>
+                                                </td>
+                                                <td class="align-middle text-center text-sm">
+                                                    <p class="text-sm font-weight-bold mb-0">{{ $membro->email }}</p>
+                                                </td>
+                                                <td class="align-middle text-center text-sm">
+                                                    <p class="text-sm font-weight-bold mb-0">{{ $membro->created_at->format('d/m/Y') }}</p>
+                                                </td>
+                                                @if(Auth::user()->clientType == 1 || Auth::user()->freelancerType == 'Product owner')
+                                                    <td class="align-middle text-end">
+                                                        <div class="d-flex px-3 py-1 justify-content-center align-items-center">
+                                                            <form action="{{ route('team-join-requests.store', ['projetoId' => $projeto->id]) }}" method="post" class="me-2">
+                                                                @csrf
+                                                                @method('post')
+                                                                <input type="hidden" name="freelancer_id" value="{{ Auth::id() }}">
+                                                                <button type="submit" class="btn btn-primary">Editar</button>
+                                                            </form>
+                                                            <form action="{{ route('equipes.remove-participante', ['membroId' => $membro->id]) }}" method="post">
+                                                                @csrf
+                                                                @method('post')
+                                                                <button type="submit" class="btn" onclick="return confirm('Deseja remover este participante?')">Remover</button>
+                                                            </form>
+                                                        </div>
+                                                    </td>
+                                                @endif
+                                            </tr>
+                                        </tbody>
+                                    @endforeach
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            @else
+                <p>O usuário não está associado a nenhum projeto.</p>
+            @endif
         </div>
     </div>
-</div>
 @endsection
